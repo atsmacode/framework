b04f3876fe102e7ab414914f4f81a4659a4d379e
Mark Ramage <mramage@hotmail.co.uk>
2022-11-27 12:26:50 +0000

    Add create/drop database migration

---
+++ database/migrations/CreateDatabase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Atsmacode\Orm\Database\Migrations;
+
+use Atsmacode\Orm\Traits\Connect;
+use PDO;
+use PDOException;
+
+class CreateDatabase
+{
+    use Connect;
+
+    public static array $methods = [
+        'dropDatabase',
+        'createDatabase'
+    ];
+
+    public function __construct()
+    {
+        $this->setCredentials();
+    }
+
+    public function dropDatabase()
+    {
+        $sql = "DROP DATABASE IF EXISTS `{$this->database}`";
+
+        try {
+            $this->connection = new PDO("mysql:host=$this->servername;", $this->username, $this->password);
+            $this->connection->exec($sql);
+        } catch(PDOException $e) {
+            error_log($e->getMessage());
+        }
+
+        return $this;
+    }
+
+    public function createDatabase()
+    {
+        $sql = "CREATE DATABASE `{$this->database}`";
+
+        try {
+            $this->connection = new PDO("mysql:host=$this->servername;", $this->username, $this->password);
+            $this->connection->exec($sql);
+        } catch(PDOException $e) {
+            error_log($e->getMessage());
+        }
+
+        return $this;
+    }
+}
