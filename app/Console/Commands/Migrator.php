281a1163d4c17958faae2a4d416198ed5e00be0a
Mark <mark@Marks-MacBook-Pro.local>
2025-03-26 06:34:18 +0000

    int/name nullable, set class props, add mac cmds

--- app/Console/Commands/Migrator.php
+++ app/Console/Commands/Migrator.php
@@ -19,10 +19,10 @@ abstract class Migrator extends Command
     protected static $defaultName;
 
     public function __construct(
-        string $name = null,
-        ServiceManager $container,
-        FactoryInterface $testDbFactory,
-        FactoryInterface $legacyTestDbFactory
+        ?string $name,
+        private ServiceManager $container,
+        private FactoryInterface $testDbFactory,
+        private FactoryInterface $legacyTestDbFactory
     ) {
         parent::__construct($name);
 
